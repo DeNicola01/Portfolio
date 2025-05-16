5773e03e5ee6243b28a77eb24ff28b7b0c0efe17
DeNicola01 <gabr7@hotmail.com>
2025-05-16 04:42:21 -0300

    primeira versao feita

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,11 @@
 import { provideRouter } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { SobreComponent } from './pages/sobre/sobre.component';
 
 export const appConfig = {
   providers: [
     provideRouter([
       { path: '', component: HomeComponent },
-      { path: 'dashboard', component: DashboardComponent },
       { path: 'sobre', component: SobreComponent },
     ])
   ]
