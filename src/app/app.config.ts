c3d551068320d1c01292ceef9e2608658449579d
DeNicola01 <gabr7@hotmail.com>
2025-05-21 08:03:35 -0300

    aba de projetos feita

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,13 @@
 import { provideRouter } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { SobreComponent } from './pages/sobre/sobre.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
 export const appConfig = {
   providers: [
     provideRouter([
       { path: '', component: HomeComponent },
+      { path: 'projects', component: ProjectsComponent},
       { path: 'sobre', component: SobreComponent },
     ])
   ]
